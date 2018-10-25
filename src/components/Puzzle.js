f02beeba1d545cfa28844e4fdaa54115540c31d3
Krystian <kordaszewskikrystian@gmail.com>
2018-10-25 12:18:48 +0200

    adding a status & guess

--- src/components/Puzzle.js
+++ src/components/Puzzle.js
@@ -7,8 +7,9 @@ class Puzzle extends Component {
 		this.state = {
 			word: '',
 			remainingGuesses: 5,
-			guessedLetters: ['a', 'e', 'o'],
-			puzzleState: ''
+			guessedLetters: [],
+			puzzleState: '',
+			status: 'playing'
 		}
 	}
 
@@ -25,10 +26,50 @@ class Puzzle extends Component {
 		this.setState({ puzzleState: puzzle })
 	}
 
+	calculateStatus() {
+		const finished = this.state.word.every(
+			letter => this.state.guessedLetters.includes(letter) || letter === ' '
+		)
+
+		if (this.state.remainingGuesses === 0) {
+			this.setState({ status: 'failed' })
+		} else if (finished) {
+			this.setState({ status: 'finished' })
+		} else {
+			this.setState({ status: 'playing' })
+		}
+	}
+
+	makeGuess(guess) {
+		guess = guess.toLowerCase()
+		const isUnique = !this.state.guessedLetters.includes(guess)
+		const isBadGuess = !this.state.word.includes(guess)
+
+		if (this.state.status !== 'playing') {
+			return
+		}
+
+		if (isUnique && isBadGuess) {
+			this.setState({ remainingGuesses: this.state.remainingGuesses - 1 })
+		}
+
+		if (isUnique) {
+			this.setState({ guessedLetters: [...this.state.guessedLetters, guess] })
+		}
+
+		this.puzzle()
+		this.calculateStatus()
+	}
+
 	async componentDidMount() {
 		const puzzleState = await getPuzzle('2')
 		this.setState({ word: puzzleState.toLowerCase().split('') })
 		this.puzzle()
+
+		window.addEventListener('keypress', e => {
+			const guess = String.fromCharCode(e.charCode)
+			this.makeGuess(guess)
+		})
 	}
 
 	render() {
