f44f4425bf128f7d42b51a732fc1b64c9e68838b
Krystian <kordaszewskikrystian@gmail.com>
2018-10-26 12:51:10 +0200

    Updates

---
+++ precache-manifest.e134e2fc3c77a9e8959353dff3a0d857.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ef55a314b93f6ce3981",
+    "url": "/hangman-react/static/js/runtime~main.3ef55a31.js"
+  },
+  {
+    "revision": "65563cc9062d1116e5b3",
+    "url": "/hangman-react/static/js/main.65563cc9.chunk.js"
+  },
+  {
+    "revision": "ea131fdecdfd184a79e1",
+    "url": "/hangman-react/static/js/1.ea131fde.chunk.js"
+  },
+  {
+    "revision": "65563cc9062d1116e5b3",
+    "url": "/hangman-react/static/css/main.c1745801.chunk.css"
+  },
+  {
+    "revision": "1903d297d0a27fe36e5b9a91c90f4205",
+    "url": "/hangman-react/index.html"
+  }
+];
